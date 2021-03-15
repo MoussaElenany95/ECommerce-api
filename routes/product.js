005e347b60a7824bf96e7ab4588371a0eeb09588
MoussaElenany95 <mousa.elenanyfciscu@gmail.com>
2021-03-15 18:29:48 +0200

    add Access-Control-Allow-Origin header to product response

--- routes/product.js
+++ routes/product.js
@@ -62,7 +62,7 @@ body('title').isLength({ min: 1 })
 //////////////////////// get all products /////////////////////////////
 
 router.get('/',auth,  async(req, res)=> {
-     
+     res.res.setHeader('Access-Control-Allow-Origin', '*');
     const products= await Product.find()
       if(products) return res.send('Products list :'+products)
  
